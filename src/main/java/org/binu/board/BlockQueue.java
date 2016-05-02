a02ae172380a5b41e158af3a92c10f798524dfeb
binu manoharan <binu1988@gmail.com>
2016-05-02 12:45:16 +0100

    Add BlockQueue and tests

--- src/main/java/org/binu/board/BlockQueue.java
+++ src/main/java/org/binu/board/BlockQueue.java
@@ -1,8 +1,35 @@
 package org.binu.board;
 
+import com.sun.istack.internal.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * BlockQueue with cells.
+ * BlockQueue with Blocks pieces that are falling on the board.
  */
 public class BlockQueue {
-    Block[] blocks;
+    private List<Block> blocks;
+
+    public BlockQueue() {
+        this.blocks = new ArrayList<Block>(8);
+    }
+
+    public void add(Block block) {
+        assert blocks.size() <= 8;
+        blocks.add(block);
+    }
+
+    @Nullable
+    public Block getNext() {
+        return blocks.size() > 0 ? blocks.get(0) : null;
+    }
+
+    public Block getNextAndPop() {
+        Block next = getNext();
+        if (next != null) {
+            blocks.remove(0);
+        }
+        return next;
+    }
 }
---
+++ src/test/java/org/binu/board/BlockQueueTest.java
@@ -0,0 +1,93 @@
+package org.binu.board;
+
+import org.binu.data.CellColour;
+import org.binu.data.CellStatus;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.*;
+
+/**
+ * Test for {@link BlockQueue}
+ */
+public class BlockQueueTest {
+
+    private BlockQueue blockQueue;
+    private Block firstBlock;
+    private Block secondBlock;
+
+    @Before
+    public void setUp() throws Exception {
+        this.blockQueue = new BlockQueue();
+    }
+
+    @Test
+    public void should_add_one_element_to_the_queue() throws Exception {
+        firstBlock = getBlock(CellColour.GREEN, CellStatus.OCCUPIED, CellColour.GREEN, CellStatus.OCCUPIED);
+        blockQueue.add(firstBlock);
+
+        assertThat("BlockQueue contains the block we just added", blockQueue.getNext(), is(firstBlock));
+    }
+
+    @Test
+    public void should_add_two_element_to_the_queue() throws Exception {
+        firstBlock = getBlock(CellColour.GREEN, CellStatus.OCCUPIED, CellColour.GREEN, CellStatus.OCCUPIED);
+        secondBlock = getBlock(CellColour.RED, CellStatus.OCCUPIED, CellColour.RED, CellStatus.OCCUPIED);
+
+        blockQueue.add(firstBlock);
+        blockQueue.add(secondBlock);
+
+        assertThat("BlockQueue contains the block we just added", blockQueue.getNext(), is(firstBlock));
+        assertThat("BlockQueue contains the block we just added", blockQueue.getNextAndPop(), is(firstBlock));
+        assertThat("BlockQueue contains the block we just added", blockQueue.getNext(), is(secondBlock));
+        assertThat("BlockQueue contains the block we just added", blockQueue.getNextAndPop(), is(secondBlock));
+        assertThat("BlockQueue contains the block we just added", blockQueue.getNextAndPop(), is(nullValue()));
+    }
+
+    @Test (expected = AssertionError.class)
+    public void should_not_be_able_to_add_more_than_max_elements_to_the_queue() throws Exception {
+        firstBlock = getBlock(CellColour.GREEN, CellStatus.OCCUPIED, CellColour.GREEN, CellStatus.OCCUPIED);
+
+        blockQueue.add(firstBlock);
+        blockQueue.add(firstBlock);
+        blockQueue.add(firstBlock);
+        blockQueue.add(firstBlock);
+        blockQueue.add(firstBlock);
+        blockQueue.add(firstBlock);
+        blockQueue.add(firstBlock);
+        blockQueue.add(firstBlock);
+        blockQueue.add(firstBlock);
+        blockQueue.add(firstBlock);
+        blockQueue.add(firstBlock);
+        blockQueue.add(firstBlock);
+
+        assertThat("BlockQueue contains the block we just added", blockQueue.getNext(), is(firstBlock));
+    }
+
+    @Test
+    public void should_return_second_element_that_was_added_to_the_queue_when_first_is_deleted() throws Exception {
+        firstBlock = getBlock(CellColour.GREEN, CellStatus.OCCUPIED, CellColour.GREEN, CellStatus.OCCUPIED);
+        secondBlock = getBlock(CellColour.RED, CellStatus.OCCUPIED, CellColour.RED, CellStatus.OCCUPIED);
+
+        blockQueue.add(firstBlock);
+        blockQueue.add(secondBlock);
+        blockQueue.getNextAndPop();
+        blockQueue.add(firstBlock);
+
+        assertThat("BlockQueue contains the block we just added", blockQueue.getNext(), is(secondBlock));
+    }
+
+    private Block getBlock(CellColour cell1Colour, CellStatus cell1Status, CellColour cell2Colour, CellStatus cell2Status) {
+        Cell[] blockCells = getCells(cell1Colour, cell1Status, cell2Colour, cell2Status);
+        return new Block(blockCells);
+    }
+
+    private Cell[] getCells(CellColour cell1Colour, CellStatus cell1Status, CellColour cell2Colour, CellStatus cell2Status) {
+        Cell[] cells = new Cell[2];
+        cells[0] = new Cell(cell1Colour, cell1Status);
+        cells[1] = new Cell(cell2Colour, cell2Status);
+        return cells;
+    }
+}
