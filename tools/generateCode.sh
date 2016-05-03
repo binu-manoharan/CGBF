b51f92e187547853893e1d2939a539a93d75b02b
Binu <binu.manoharan@i-nexus.com>
2016-05-03 12:48:58 +0100

    Updated files

--- tools/generateCode.sh
+++ tools/generateCode.sh
@@ -1,4 +1,35 @@
 #/bin/bash
 
-PROJ_HOME='/home/binu/playground/CGBF/tools'
-echo $PROJ_HOME
+PROJ_HOME='/home/binu/playground/CGBF/'
+#echo $PROJ_HOME
+
+OUT_FILE='tools/MergedPlayer.out'
+#echo $OUT_FILE
+
+PLAYER_FILE='tools/Player.java'
+
+SRC_MAIN='src/main/java/'
+
+outFilePath=$PROJ_HOME$OUT_FILE
+playerFilePath=$PROJ_HOME$PLAYER_FILE
+srcMainPath=$PROJ_HOME$SRC_MAIN
+
+if [[ -f $outFilePath ]];
+then
+    echo "File exists... removing."
+    rm $outFilePath
+fi
+
+cat $playerFilePath >> $outFilePath
+
+for i in `find $srcMainPath -type f | grep java$`
+do
+    echo "File names "$i
+    cat $i >> $outFilePath
+done
+
+
+
+
+
+
