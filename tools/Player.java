1cbb4b2b1b4e9567719c46e06f48776c449475c4
Binu <binu.manoharan@i-nexus.com>
2016-05-03 12:24:50 +0100

    Adding player file

---
+++ tools/Player.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        // game loop
+        while (true) {
+            for (int i = 0; i < 8; i++) {
+                int colorA = in.nextInt(); // color of the first block
+                int colorB = in.nextInt(); // color of the attached block
+            }
+            for (int i = 0; i < 12; i++) {
+                String row = in.next();
+            }
+            for (int i = 0; i < 12; i++) {
+                String row = in.next(); // One line of the map ('.' = empty, '0' = skull block, '1' to '5' = colored block)
+            }
+
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+            System.out.println("0"); // "x": the column in which to drop your blocks
+        }
+    }
+}
