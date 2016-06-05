84814e4abd6def8335ea83ee2617284ab99f397a
binu manoharan <binu1988@gmail.com>
2016-06-05 11:30:05 +0100

    Add Orientation for rotation.

---
+++ src/main/java/org/binu/data/Orientation.java
@@ -0,0 +1,15 @@
+package org.binu.data;
+
+/**
+ * Enum for Rotation of the block
+ * HORIZONTAL 1 -> 4
+ * VERTCIAL 1 below 4
+ * HORIZONTAL_REVERSED 4 -> 1
+ * VERTICAL_REVERSED 4 below 1
+ */
+public enum Orientation {
+    HORIZONTAL,
+    VERTICAL,
+    HORIZONTAL_REVERSED,
+    VERTICAL_REVERSED
+}
